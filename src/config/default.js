b00ccc33582a820810e9d15a8f27e515f7e63522
ylpxzx <794859685@qq.com>
2025-03-04 12:09:34 +0800

    feat: 窗口默认大小修改

--- src/config/default.js
+++ src/config/default.js
@@ -2,8 +2,8 @@ export const config = {
   mainWindow: {
     x: 0,
     y: 0,
-    width: 600,
-    height: 500,
+    width: 300,
+    height: 400,
     minWidth: 300,
     minHeight: 400,
     isAlwaysOnTop: false,
